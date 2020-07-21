c4a8f90879f6783463c83210db1b67ee3e0d6375
kent <kent.liu.philippines@gmail.com>
2020-07-21 21:52:09 +0800

    【删除】多余文件 【新增】用户修改接口

--- homex-api/src/main/java/homex/api/UserApi.java
+++ homex-api/src/main/java/homex/api/UserApi.java
@@ -56,4 +56,28 @@ public class UserApi extends BaseController{
 		return Result.buildFailMessage("登录失败");
 	}
 	
+	/**
+	 * <p>修改用户数据</p>
+	 * @param userID				用户唯一标识			
+	 * @param userName				用户名
+	 * @param userAvtar				用户头像，base64文件
+	 * @param userEmail				用户邮箱
+	 * @param userPhone				用户手机号
+	 * @param password				密码
+	 * @return
+	 */
+	@PutMapping("/editAccount")
+	public Result editAccount(
+			String userID 
+			, String userName 
+			, String userAvtar 
+			, String userEmail 
+			, String userPhone 
+			, String password 
+			) {
+				return null;
+	}
+	
+	
+	
 }
